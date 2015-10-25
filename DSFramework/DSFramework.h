381a0694dcbc50ebd270626401dd2f7da799b980
kujenga <ataylor0123@gmail.com>
2015-10-24 22:36:15 -0400

    configured for double copies of stdlib

---
+++ DSFramework/DSFramework.h
@@ -0,0 +1,19 @@
+//
+//  DSFramework.h
+//  DSFramework
+//
+//  Created by Aaron Taylor on 10/24/15.
+//  Copyright © 2015 Aaron Taylor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DSFramework.
+FOUNDATION_EXPORT double DSFrameworkVersionNumber;
+
+//! Project version string for DSFramework.
+FOUNDATION_EXPORT const unsigned char DSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSFramework/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+workspace 'DoubleSwift'
+xcodeproj 'DoubleSwift.xcodeproj'
+
+link_with 'DSFramework'
+
+use_frameworks!
+inhibit_all_warnings!
+
+pod 'Alamofire', '~> 3.0'

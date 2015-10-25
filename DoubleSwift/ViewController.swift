a3e29cc0fc8dbfabd203ae6ed5d514b3265d1a5f
kujenga <ataylor0123@gmail.com>
2015-10-24 21:47:56 -0400

    Initial Commit

---
+++ DoubleSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DoubleSwift
+//
+//  Created by Aaron Taylor on 10/24/15.
+//  Copyright © 2015 Aaron Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
